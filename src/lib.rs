ac752706fabc24e67eede2d50325da9d2d74ed60
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-01-15 18:45:31 -0500

    project setup

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![warn(clippy::wildcard_imports)]
+#![warn(clippy::needless_pass_by_ref_mut)]
+#![deny(clippy::missing_docs_in_private_items)]
+#![deny(missing_docs)]
+#![doc = include_str!("../README.md")]
+
+mod noise;
