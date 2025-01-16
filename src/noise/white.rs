d9fa684f34ac8fe94da3b1193c727ce7d0d6fdc6
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-01-16 00:28:44 -0500

    improved white noise

--- src/noise/white.rs
+++ src/noise/white.rs
@@ -35,8 +35,7 @@ macro_rules! impl_white {
                     let inner: $conv = input.into();
                     #[allow(for_loops_over_fallibles)] // this lets you use option to work on just one input
                     for v in inner {
-                        // multiplication should be pipelined as a vector for v. xor to combine.
-                        val = v.wrapping_mul($key).bitxor(val)
+                        val += v.wrapping_add($key).wrapping_mul($key) // this should be pipelined pretty well since the addition is only at the very end.
                     }
                     val.rotate_left(5) // multiplying large numbers like this tends to put more entorpy on the more significant bits. This pushes that entropy to the least segnificant.
                 }
@@ -49,7 +48,7 @@ macro_rules! impl_white {
 impl_white!(
     u8,
     White8,
-    251,
+    97,
     (u8, Option<u8>),
     (U8Vec2, [u8; 2]),
     (U8Vec3, [u8; 3]),
@@ -62,7 +61,7 @@ impl_white!(
 impl_white!(
     u16,
     White16,
-    65521,
+    1777,
     (u16, Option<u16>),
     (U16Vec2, [u16; 2]),
     (U16Vec3, [u16; 3]),
@@ -75,7 +74,7 @@ impl_white!(
 impl_white!(
     u32,
     White32,
-    4294967279,
+    104_395_303,
     (u32, Option<u32>),
     (UVec2, [u32; 2]),
     (UVec3, [u32; 3]),
@@ -88,7 +87,7 @@ impl_white!(
 impl_white!(
     u64,
     White64,
-    18446744073709551557,
+    982_451_653,
     (u64, Option<u64>),
     (U64Vec2, [u64; 2]),
     (U64Vec3, [u64; 3]),
@@ -103,7 +102,7 @@ impl_white!(
 impl_white!(
     usize,
     WhiteUsize,
-    4294967279,
+    104_395_303,
     (usize, Option<usize>),
     ([usize; 2], [usize; 2]),
     ([usize; 3], [usize; 3]),
@@ -114,7 +113,7 @@ impl_white!(
 impl_white!(
     usize,
     WhiteUsize,
-    18446744073709551557,
+    982_451_653,
     (usize, Option<usize>),
     ([usize; 2], [usize; 2]),
     ([usize; 3], [usize; 3]),
