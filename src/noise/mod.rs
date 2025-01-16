a318126e5f947e9939394bf71a801750af88781a
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-01-15 23:42:16 -0500

    added norm values

--- src/noise/mod.rs
+++ src/noise/mod.rs
@@ -15,3 +15,33 @@ pub trait Noise<Input> {
         self.sample(input)
     }
 }
+
+/// A value that stores an f32 in range (-1, 0)∪(0, 1).
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct SNorm(f32);
+
+/// A value that stores an f32 in range (0, 1).
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct UNorm(f32);
+
+impl SNorm {
+    /// constructs an arbetrary but valid value using these bits
+    pub fn from_bits(bits: u32) -> Self {
+        // the following is inspired by rand's Open01 implementation:
+
+        use rand::distributions::hidden_export::IntoFloat;
+        // Transmute-based method; 23/52 random bits; (0, 1) interval.
+        // We use the most significant bits because for simple RNGs
+        // those are usually more random.
+        let raw = bits.into_float_with_exponent(0) - (1.0 - f32::EPSILON / 2.0);
+        Self(raw)
+    }
+}
+
+impl UNorm {
+    /// constructs an arbetrary but valid value using these bits
+    pub fn from_bits(mut bits: u32) -> Self {
+        bits &= !(1 << 31); // ensures float's sign bit isn't on
+        Self(SNorm::from_bits(bits).0)
+    }
+}
