234ca084f86c13b768c59e3194782916920f4138
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-11 23:48:43 -0500

    improved white noise
    slightly faster now for single values and better quality too.

--- src/noise/white.rs
+++ src/noise/white.rs
@@ -24,22 +24,71 @@ macro_rules! impl_white {
         #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
         pub struct $name(pub $dt);
 
+        impl<const N: usize> NoiseOp< [$dt; N] > for $name {
+            type Output = $dt;
+
+            #[inline(always)]
+            fn get(&self, input: [$dt; N]) -> Self::Output {
+                let slice: &[$dt] = &input;
+                self.get(slice)
+            }
+        }
+
+        impl NoiseOp< Vec<$dt> > for $name {
+            type Output = $dt;
+
+            #[inline(always)]
+            fn get(&self, input: Vec<$dt>) -> Self::Output {
+                let slice: &[$dt] = &input;
+                self.get(slice)
+            }
+        }
+
+        impl NoiseOp< Option<$dt> > for $name {
+            type Output = $dt;
+
+            #[inline(always)]
+            fn get(&self, input: Option<$dt>) -> Self::Output {
+                if let Some(input) = input {
+                    self.get([input])
+                } else {
+                    $key
+                }
+            }
+        }
+
+        impl NoiseOp<$dt> for $name {
+            type Output = $dt;
+
+            #[inline(always)]
+            fn get(&self, input: $dt) -> Self::Output {
+                    (input ^ self.0) // salt with the seed
+                    .wrapping_mul($key) // multiply to remove any linear artifacts
+                    .rotate_left(5) // multiplying large numbers like this tends to put more entropy on the more significant bits. This pushes that entropy to the least segnificant.
+            }
+        }
+
+        impl NoiseOp<&'_ [$dt]> for $name {
+            type Output = $dt;
+
+            #[inline(always)]
+            fn get(&self, input: &[$dt]) -> Self::Output {
+                let mut val: $dt = $key;
+                for v in input {
+                    val = v.wrapping_mul(val) ^ $key // need xor to make it non-commutative to remove diagonal lines and multiplication to put each dimension on separate roders
+                }
+                self.get(val)
+            }
+        }
+
         $(
             impl NoiseOp< $input > for $name {
                 type Output = $dt;
 
                 #[inline(always)]
                 fn get(&self, input: $input) -> Self::Output {
-                    let mut val: $dt = $key;
                     let inner: $conv = input.into();
-                    #[allow(for_loops_over_fallibles)] // this lets you use option to work on just one input
-                    for v in inner {
-                        val = v.wrapping_mul(val) ^ $key // need xor to make it non-commutative to remove diagonal lines and multiplication to put each dimension on separate roders
-                    }
-                    val
-                        .wrapping_add(self.0) // salt with the seed
-                        .wrapping_mul(val) // multiply to remove any linear artifacts
-                        .rotate_left(5) // multiplying large numbers like this tends to put more entropy on the more significant bits. This pushes that entropy to the least segnificant.
+                    self.get(inner)
                 }
             }
         )*
@@ -51,88 +100,41 @@ impl_white!(
     u8,
     White8,
     97,
-    (u8, Option<u8>),
     (U8Vec2, [u8; 2]),
     (U8Vec3, [u8; 3]),
     (U8Vec4, [u8; 4]),
-    ([u8; 2], [u8; 2]),
-    ([u8; 3], [u8; 3]),
-    ([u8; 4], [u8; 4]),
-    (Vec<u8>, Vec<u8>),
 );
 impl_white!(
     u16,
     White16,
     1777,
-    (u16, Option<u16>),
     (U16Vec2, [u16; 2]),
     (U16Vec3, [u16; 3]),
     (U16Vec4, [u16; 4]),
-    ([u16; 2], [u16; 2]),
-    ([u16; 3], [u16; 3]),
-    ([u16; 4], [u16; 4]),
-    (Vec<u16>, Vec<u16>),
 );
 impl_white!(
     u32,
     White32,
     104_395_303,
-    (u32, Option<u32>),
     (UVec2, [u32; 2]),
     (UVec3, [u32; 3]),
     (UVec4, [u32; 4]),
-    ([u32; 2], [u32; 2]),
-    ([u32; 3], [u32; 3]),
-    ([u32; 4], [u32; 4]),
-    (Vec<u32>, Vec<u32>),
 );
 impl_white!(
     u64,
     White64,
     982_451_653,
-    (u64, Option<u64>),
     (U64Vec2, [u64; 2]),
     (U64Vec3, [u64; 3]),
     (U64Vec4, [u64; 4]),
-    ([u64; 2], [u64; 2]),
-    ([u64; 3], [u64; 3]),
-    ([u64; 4], [u64; 4]),
-    (Vec<u64>, Vec<u64>),
 );
 
-impl_white!(
-    u128,
-    White128,
-    982_451_653_011,
-    (u128, Option<u128>),
-    ([u128; 2], [u128; 2]),
-    ([u128; 3], [u128; 3]),
-    ([u128; 4], [u128; 4]),
-    (Vec<u128>, Vec<u128>),
-);
+impl_white!(u128, White128, 982_451_653_011,);
 
 #[cfg(target_pointer_width = "32")]
-impl_white!(
-    usize,
-    WhiteUsize,
-    104_395_303,
-    (usize, Option<usize>),
-    ([usize; 2], [usize; 2]),
-    ([usize; 3], [usize; 3]),
-    ([usize; 4], [usize; 4]),
-    (Vec<usize>, Vec<usize>),
-);
+impl_white!(usize, WhiteUsize, 104_395_303,);
 #[cfg(target_pointer_width = "64")]
-impl_white!(
-    usize,
-    WhiteUsize,
-    982_451_653,
-    (usize, Option<usize>),
-    ([usize; 2], [usize; 2]),
-    ([usize; 3], [usize; 3]),
-    ([usize; 4], [usize; 4]),
-    (Vec<usize>, Vec<usize>),
-);
+impl_white!(usize, WhiteUsize, 982_451_653,);
 
 #[cfg(test)]
 mod tests {
