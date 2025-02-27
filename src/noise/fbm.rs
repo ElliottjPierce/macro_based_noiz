c5d6fedee7bb86ec0a0d5da1da3c7f2e7e6e79f9
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-26 21:18:56 -0500

    basic implementation

--- src/noise/fbm.rs
+++ src/noise/fbm.rs
@@ -1,5 +1,7 @@
 //! This module allows factional brownian motion (fbm) noise.
 
+use std::marker::PhantomData;
+
 use super::{
     NoiseOp,
     NoiseType,
@@ -7,19 +9,13 @@ use super::{
 
 /// Signifies that this type can be the result of an fbm octave.
 pub trait FbmOctaveResult: NoiseType {
-    /// The final result type of the octave
-    type Result: NoiseType;
-
-    /// Converts this type to [`Result`](FbmOctaveResult::Result) by some `contribution` in (0,1).
+    /// Scales this result by some octave `contribution` in (0,1).
     /// Usually this is just multiplication.
-    fn fit_contribution(self, contribution: f32) -> Self::Result;
+    fn fit_contribution(&mut self, contribution: f32);
 }
 
 /// Marks this noise type as being able to be used as an octave of fbm noise.
-pub trait FbmOctaveNoise<I: NoiseType, D>: NoiseOp<I>
-where
-    Self::Output: FbmOctaveResult,
-{
+pub trait FbmOctaveNoise<I: NoiseType, D>: NoiseOp<I, Output: FbmOctaveResult> {
     /// This creates the octave based on some input data.
     fn create_octave(data: D) -> Self;
 }
@@ -39,8 +35,39 @@ pub trait FbmSettings<D> {
 }
 
 /// Stores an octave of fbm for some [`FbmOctaveNoise`], `D`.
-pub struct FmbOctave<N> {
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct FbmOctave<N> {
     noise: N,
     /// The octave's contribution in (0,1)
     contribution: f32,
+}
+
+impl<I: NoiseType, N: NoiseOp<I, Output: FbmOctaveResult>> NoiseOp<I> for FbmOctave<N> {
+    type Output = N::Output;
+
+    #[inline]
+    fn get(&self, input: I) -> Self::Output {
+        let mut result = self.noise.get(input);
+        result.fit_contribution(self.contribution);
+        result
+    }
+}
+
+/// Fbm noise itself.
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct FBM<T, F>(T, PhantomData<F>);
+
+impl<
+    D,
+    I: NoiseType + Clone,
+    N0: FbmOctaveNoise<I, D>,
+    N1: FbmOctaveNoise<I, D, Output = N0::Output>,
+> NoiseOp<I> for FBM<(FbmOctave<N0>, FbmOctave<N1>), D>
+{
+    type Output = [N0::Output; 2];
+
+    #[inline]
+    fn get(&self, input: I) -> Self::Output {
+        [self.0.0.get(input.clone()), self.0.1.get(input.clone())]
+    }
 }
