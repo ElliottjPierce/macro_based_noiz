23b8fd1a1a52c94c6a5d275866b06a7555ac547c
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-26 21:38:19 -0500

    full but simple implementation

--- src/noise/fbm.rs
+++ src/noise/fbm.rs
@@ -1,7 +1,5 @@
 //! This module allows factional brownian motion (fbm) noise.
 
-use std::marker::PhantomData;
-
 use super::{
     NoiseOp,
     NoiseType,
@@ -14,16 +12,12 @@ pub trait FbmOctaveResult: NoiseType {
     fn fit_contribution(&mut self, contribution: f32);
 }
 
-/// Marks this noise type as being able to be used as an octave of fbm noise.
-pub trait FbmOctaveNoise<I: NoiseType, D>: NoiseOp<I, Output: FbmOctaveResult> {
-    /// This creates the octave based on some input data.
-    fn create_octave(data: D) -> Self;
-}
-
 /// Allows this type to generate fbm octaves.
 pub trait FbmOctaveGenerator<D> {
     /// Gets the next octave initializer.
     fn get_octave(&self) -> D;
+    /// Gets the weight/influence of this octave.
+    fn get_weight(&self) -> f32;
     /// Updates self to prepare the next octave.
     fn progress_octave(&mut self);
 }
@@ -31,7 +25,7 @@ pub trait FbmOctaveGenerator<D> {
 /// Represents settings that can be used to make fmb.
 pub trait FbmSettings<D> {
     /// Uses these settings to construct an [`FbmOctaveGenerator`]
-    fn get_generator(&self, octaves: u32) -> impl FbmOctaveGenerator<D>;
+    fn get_generator(&self, octaves: u8) -> impl FbmOctaveGenerator<D>;
 }
 
 /// Stores an octave of fbm for some [`FbmOctaveNoise`], `D`.
@@ -53,21 +47,60 @@ impl<I: NoiseType, N: NoiseOp<I, Output: FbmOctaveResult>> NoiseOp<I> for FbmOct
     }
 }
 
+impl<N> FbmOctave<N> {
+    /// constructs a new [`FbmOctave`] where the `contribution` has not yet been normalized.
+    #[inline]
+    fn new_octave_partial<D>(
+        generator: &mut impl FbmOctaveGenerator<D>,
+        total_contribution: &mut f32,
+    ) -> Self
+    where
+        N: From<D>,
+    {
+        let contribution = generator.get_weight();
+        *total_contribution += contribution;
+        let result = Self {
+            noise: generator.get_octave().into(),
+            contribution,
+        };
+        generator.progress_octave();
+        result
+    }
+}
+
 /// Fbm noise itself.
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
-pub struct FBM<T, F>(T, PhantomData<F>);
+pub struct Fbm<T>(T);
 
 impl<
-    D,
     I: NoiseType + Clone,
-    N0: FbmOctaveNoise<I, D>,
-    N1: FbmOctaveNoise<I, D, Output = N0::Output>,
-> NoiseOp<I> for FBM<(FbmOctave<N0>, FbmOctave<N1>), D>
+    N0: NoiseOp<I, Output: FbmOctaveResult>,
+    N1: NoiseOp<I, Output = N0::Output>,
+> NoiseOp<I> for Fbm<(FbmOctave<N0>, FbmOctave<N1>)>
 {
     type Output = [N0::Output; 2];
 
     #[inline]
     fn get(&self, input: I) -> Self::Output {
         [self.0.0.get(input.clone()), self.0.1.get(input.clone())]
+    }
+}
+
+impl<N0, N1> Fbm<(FbmOctave<N0>, FbmOctave<N1>)> {
+    /// Constructs a new [`FBM`] given these settings.
+    pub fn new_fbm<D, G: FbmOctaveGenerator<D>>(settings: &impl FbmSettings<D>) -> Self
+    where
+        N0: From<D>,
+        N1: From<D>,
+    {
+        let mut generator = settings.get_generator(2);
+        let mut total_contribution = 0.0;
+        let mut result = Self((
+            FbmOctave::new_octave_partial(&mut generator, &mut total_contribution),
+            FbmOctave::new_octave_partial(&mut generator, &mut total_contribution),
+        ));
+        result.0.0.contribution /= total_contribution;
+        result.0.1.contribution /= total_contribution;
+        result
     }
 }
