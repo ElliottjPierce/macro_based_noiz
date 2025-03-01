f09410dd128bc96f335dc693eb5f69787b2635e2
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-03-01 14:35:25 -0500

    unchecked fbm

--- src/noise/fbm.rs
+++ src/noise/fbm.rs
@@ -3,10 +3,19 @@
 /// Represents the settings of fbm of a certain type of octave.
 pub trait FbmSettings {
     /// The type of octave used.
-    type Octave;
+    type Octave: FbmOctave;
 
     /// For `N` octaves, gets each octave.
     fn get_octaves<const N: usize>(self) -> [Self::Octave; N];
+}
+
+/// Represents a layer of fbm.
+pub trait FbmOctave {
+    /// The data that is actually stored for reuse.
+    type Stored;
+
+    /// Converts this value to its storage.
+    fn finish(self) -> Self::Stored;
 }
 
 /// Starts a corresponding [`FbmAccumulator`] for `N` octaves.
@@ -37,4 +46,24 @@ pub trait ContributoryOctave<T> {
 
     /// Fits the value to the output based on the octave's contents.
     fn fit_contribution(&self, value: T) -> Self::Output;
+}
+
+/// Fbm that is completely unchecked. This lets you do whatever you want, including custom octaves
+/// and layers, etc.
+pub struct UncheckedFbm;
+
+impl FbmSettings for UncheckedFbm {
+    type Octave = ();
+
+    fn get_octaves<const N: usize>(self) -> [Self::Octave; N] {
+        [(); N]
+    }
+}
+
+impl FbmOctave for () {
+    type Stored = Self;
+
+    fn finish(self) -> Self::Stored {
+        self
+    }
 }
